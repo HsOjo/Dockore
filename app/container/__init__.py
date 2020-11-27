99992306ceac7566ef7630510211f8ff745bc0e9
HsOjo <1134031392@qq.com>
2020-11-27 10:35:41 +0800

    Add image/container view api.

--- app/container/__init__.py
+++ app/container/__init__.py
@@ -1,3 +1,5 @@
+from flask import request
+
 from app import common
 from app.base.user_api_controller import UserAPIController
 
@@ -11,13 +13,27 @@ class Container(UserAPIController):
 
     def callback_add_routes(self):
         self.add_route('/list', self.list, methods=['POST'])
+        self.add_route('/item/<string:id>', self.item, methods=['POST'])
 
     def list(self):
+        try:
+            data = request.get_json()  # type: dict
+            is_all = data.get('is_all', True)
+        except:
+            raise self.ParamsNotMatchException
+
         docker = common.get_docker_cli()
         items = [{
             'id': container.attrs['Id'],
             'name': container.attrs['Name'],
             'image_id': container.attrs['Image'],
             'create_time': container.attrs['Created'],
-        } for container in docker.containers.list()]
+        } for container in docker.containers.list(
+            all=is_all
+        )]
         return self.make_response(items=items)
+
+    def item(self, id: str):
+        docker = common.get_docker_cli()
+        item = docker.containers.get(id)
+        return self.make_response(item=item.attrs)
--- app/image/__init__.py
+++ app/image/__init__.py
@@ -1,3 +1,5 @@
+from flask import request
+
 from app import common
 from app.base.user_api_controller import UserAPIController
 
@@ -11,8 +13,15 @@ class Image(UserAPIController):
 
     def callback_add_routes(self):
         self.add_route('/list', self.list, methods=['POST'])
+        self.add_route('/item/<string:id>', self.item, methods=['POST'])
 
     def list(self):
+        try:
+            data = request.get_json()  # type: dict
+            is_all = data.get('is_all', False)
+        except:
+            raise self.ParamsNotMatchException
+
         docker = common.get_docker_cli()
         items = [{
             'id': img.attrs['Id'],
@@ -20,5 +29,12 @@ class Image(UserAPIController):
             'create_time': img.attrs['Created'],
             'size': img.attrs['Size'],
             'comment': img.attrs['Comment'],
-        } for img in docker.images.list()]
+        } for img in docker.images.list(
+            all=is_all
+        )]
         return self.make_response(items=items)
+
+    def item(self, id: str):
+        docker = common.get_docker_cli()
+        item = docker.images.get(id)
+        return self.make_response(item=item.attrs)
