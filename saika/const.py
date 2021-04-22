43dae550484f24387847f8355371eb62fcfe9b70
HsOjo <1134031392@qq.com>
2021-04-22 13:38:52 +0800

    Seperate saika.

--- saika/const.py
+++
@@ -1,4 +0,0 @@
-class Const:
-    version = '0.0.0'
-    config_file = 'config.json'
-    data_dir = 'data'
--- saika/environ.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-
-class Environ:
-    app: Flask = None
-    program_path: str
-    config_path: str
-    data_path: str
