a9480d34b5f3c576d33e3b76da8045040d559e20
HsOjo <1134031392@qq.com>
2022-10-10 00:49:01 +0800

    [chore] 适配新版本框架

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from saika import SaikaApp
+from saika.form import set_form_validate_default
+from saika.workers import set_fork_killer
 
 
 class Application(SaikaApp):
+    program_rel_path = '..'
+    app_module = 'app'
+
     def callback_init_app(self):
-        self.set_form_validate_default(True)
-        self.set_fork_killer(False)
+        set_form_validate_default(True)
+        set_fork_killer(False)
 
 
 app = Application()
